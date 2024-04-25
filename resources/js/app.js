e587483f61c62b68fd703c56ac6b39368f98a77f
原澤 直也 <rururulily@gmail.com>
2024-04-26 00:11:15 +0900

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import '../css/app.css';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+createApp(App).mount("#app");
---
+++ resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>{{ counter }}</p>
+        <button @click="counter += 1">Click!</button>
+    </div>
+</template>
+
+<script setup>
+    const counter = defineModel({ default: 0 });
+</script>
