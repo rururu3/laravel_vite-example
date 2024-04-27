cf1ab153b2e4b549d0739bd8ba717361559dffe9
原澤 直也 <rururulily@gmail.com>
2024-04-27 21:47:50 +0900

    チャットメッセージ表示

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div>
-    <p>{{ counter }}</p>
-    <button @click="counter += 1">Click!</button>
-
-    <div class="q-pa-md q-gutter-sm">
-      <q-btn push color="white" text-color="primary" label="Unread Mails">
-        <q-badge color="orange" floating>22</q-badge>
-      </q-btn>
-
-      <q-btn dense color="purple" round icon="email" class="q-ml-md">
-        <q-badge color="red" floating>4</q-badge>
-      </q-btn>
-    </div>
+    <ChatItem v-for="item of items" :chatdata="item"></ChatItem>
   </div>
 </template>
 
 <script setup>
-  const counter = defineModel({ default: 0 });
+  import { ref } from 'vue'
+  import ChatItem from './ChatItem.vue'
+
+  const items = ref([]);
 
   window.Echo.channel("test-channel").listen("TestEvent", function (e) {
-      console.log("vue: ", e.message);
+    items.value.push({
+      name: 'name',
+      title: 'title',
+      message: 'message',
+      date: 'date'
+    });
   });
 </script>
---
+++ resources/js/components/ChatItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <q-list bordered class="rounded-borders" style="max-width: 350px">
+      <q-item-label header>Friends</q-item-label>
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">{{ chatdata.title }}</q-item-label>
+          <q-item-label caption lines="2">
+            <span class="text-weight-bold">{{ chatdata.name }}</span>
+            {{ chatdata.message }}
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          {{ chatdata.date }}
+        </q-item-section>
+      </q-item>
+
+      <q-separator inset="item" />
+    </q-list>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    chatdata: Object,
+  })
+</script>
